aa14672884904ba83c2a1d22f8d4f78726d8775b
Tarek Steele <tarek@my-little-tauntaun.de>
2012-03-03 21:42:42 +0100

    1

---
+++ index.php
@@ -0,0 +1,25 @@
+<? 
+    require_once 'php/main.php';  
+
+    new Main();
+/*
+<!DOCTYPE HTML>
+<html>
+    <head>
+	<
+    </head>
+    <body>
+    	<iframe id="target" name="target"></iframe>
+        <div id="wrapper">
+        	<table width="100%">
+				<!-- header-->
+				<?  include 'php/header.php'; ?>
+                <!-- content -->
+				<?  ?>
+            </table>
+        </div>
+    </body>
+</html>
+*/
+
+?>
---
+++ php/main.php
@@ -0,0 +1,11 @@
+<?
+	/**
+	 *   The main class handling the complete process. Created by the index.php.
+	 */
+	class Main {
+		function __construct() {
+			echo "Darp";
+		}
+
+	}
+?>
